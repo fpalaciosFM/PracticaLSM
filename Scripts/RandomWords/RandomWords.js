882e72f5ae0681e94a8cf2ef469b837a3399b6b6
Fernando Palacios <fernando.palacios@ids.com.mx>
2023-10-13 13:24:26 -0600

    json test 2

--- Scripts/RandomWords/RandomWords.js
+++ Scripts/RandomWords/RandomWords.js
@@ -1,10 +1,10 @@
-const response = await fetch('https://raw.githubusercontent.com/fpalaciosFM/PracticaLSM/VectorTest/Resources/Json/RandomWords/word_list_json.json');
+const response = await fetch('https://raw.githubusercontent.com/fpalaciosFM/PracticaLSM/VectorTest/Resources/Json/RandomWords/word_list.json');
 
 if(!response.ok){
     throw new Error('Error! status: ${response.status}');
 }
 
-const word_list_json = response.json();
+const word_list_json = await response.json();
 console.log(JSON.stringify(word_list_json));
 word_list = JSON.parse(JSON.stringify(word_list_json))
 const item = word_list.word_list[Math.floor(Math.random()*word_list.word_list.length)];
