512c7de2d52d2999a249efa531a877d33c913aa4
Fernando Palacios <fernando.palacios@ids.com.mx>
2023-10-13 10:45:03 -0600

    RandomWords json fix 3

--- Scripts/RandomWords/RandomWords.js
+++ Scripts/RandomWords/RandomWords.js
@@ -1,3 +1,3 @@
-fetch('https://raw.githubusercontent.com/fpalaciosFM/PracticaLSM/VectorTest/Resources/Json/RandomWords/RandomWords.json')
+fetch('https://raw.githubusercontent.com/fpalaciosFM/PracticaLSM/VectorTest/Resources/Json/RandomWords/word_list.json')
     .then((response) => response.json())
     .then((json) => console.log(json))
