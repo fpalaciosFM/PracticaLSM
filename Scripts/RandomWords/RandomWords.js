7f022b62cc2be24a31126a76a139488f456dd2fd
Fernando Palacios <fernando.palacios@ids.com.mx>
2023-10-13 13:02:04 -0600

    Link pagina palabras aleatorias.

--- Scripts/RandomWords/RandomWords.js
+++ Scripts/RandomWords/RandomWords.js
@@ -1,3 +1,8 @@
-fetch('https://raw.githubusercontent.com/fpalaciosFM/PracticaLSM/VectorTest/Resources/Json/RandomWords/word_list.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json))
+var response = fetch('https://raw.githubusercontent.com/fpalaciosFM/PracticaLSM/VectorTest/Resources/Json/RandomWords/word_list.json');
+var word_list = response.json();
+
+console.log(word_list);
+
+var item = word_list[Math.floor(Math.random()*word_list.length)];
+
+document.getElementById("word").value = item.id;
